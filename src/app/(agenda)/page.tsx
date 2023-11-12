93035793c3e3a725a17898aa8c76886eeb32fbbf
paco9595 <paco.villarreal9555@gmail.com>
2023-11-11 23:13:37 -0600

    merge conflicts

--- src/app/(agenda)/page.tsx
+++ src/app/(agenda)/page.tsx
@@ -17,17 +17,12 @@ export default async function Home() {
   } = await supabase.auth.getUser();
 
   if (!user) return redirect('/login')
-  // let query = supabase
-  //   .from("cita")
-  //   .select("nombre, tipo, date, notas, paciente(*)");
-
-  // let { data, error }: DbResult<typeof query> = await query;
-
-  // console.log({ data, error });
+  
+  const { data } = await supabase.from("cita").select();
 
   return (
     <div className="px-5 grid gap-x-2 grid-cols-1 grid-rows-[90px_100%_1fr] lg:grid-cols-[2fr_1fr] lg:grid-rows-1 h-screen">
-      <MonthCalendar eventes={[]} />
+      <MonthCalendar eventes={data} />
       <Uncomminglist />
     </div>
   );
