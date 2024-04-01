280bf195e04a5ebff960bffdbed5e6ad66cdfa2f
paco9595 <paco.villarreal9555@gmail.com>
2024-04-01 12:24:43 -0600

    create Header component

---
+++ src/components/header/notifiacion.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function Notifiacion() {
+  return (
+    <DropdownMenu >
+      <DropdownMenuTrigger >
+      <Image
+            src={`/icons/logout.svg`}
+            alt={`notification Icon`}
+            width={12}
+            height={12}
+            className="md:w-5 md:h-5"
+          />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
